7018c7c099787a57caa19200bcd60bb97e58f5fe
40777 <40777@PC004147.coll.lr.local>
2014-12-12 14:30:44 +0000

    List starter

---
+++ task 3 list starter.py
@@ -0,0 +1,14 @@
+# christopher
+#12.12.2014
+# list starter :D
+
+
+values = 0,24,13,57,45
+result = 0
+index = 0
+for index in range (4):
+    index = index + 1
+    if result  < values[index]:
+        result = values[index]
+
+print (result)
